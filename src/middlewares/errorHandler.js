a07e0d4b5a691bd3d97c165b4e8514ed8403e2ef
Yevhen Shymka <schimka3@gmail.com>
2025-09-21 11:21:41 +0200

    fix: update ESLint config for Express middleware
    - Configure ESLint to ignore unused args in functions (Express middleware)
    - Fix no-unused-vars warning in errorHandler
    - Professional ESLint setup for Express projects

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,3 +1,5 @@
+// Express error middleware має містити точно 4 параметри
+// eslint-disable-next-line no-unused-vars
 const errorHandler = (err, req, res, next) => {
   const { status = 500, message = 'Something went wrong' } = err;
 
