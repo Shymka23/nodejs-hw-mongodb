b6c63cb8f43385cd9fbd73b3cfa597027560f5e9
Yevhen Shymka <schimka3@gmail.com>
2025-10-16 20:52:14 +0200

    Add Swagger documentation with Redocly preview

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 
 import contactsRouter from './routers/contacts.js';
 import authRouter from './routers/auth.js';
+import apiDocsRouter from './routers/apiDocs.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
@@ -27,6 +28,7 @@ export const startServer = () => {
 
   // Routes
   app.use('/auth', authRouter);
+  app.use('/api-docs', apiDocsRouter);
   app.use(contactsRouter);
 
   // 404 handler for non-existent routes
